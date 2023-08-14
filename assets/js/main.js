143818a8c456a39a30cef76a80dd3a7156a96a32
ruks2022 <https://github.com/ruks2022/instagram_clone.git>
2023-08-14 18:51:49 +0530

    home

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// AOS.init();
+
+/*Trending Auction Cards*/
+$(document).ready(function() {
+    // Swiper: Slider
+        new Swiper('.swiper-container', {
+            loop: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            slidesPerView: 1,
+            paginationClickable: true,
+            spaceBetween: 20,
+            breakpoints: {
+                1920: {
+                    slidesPerView: 3,
+                    spaceBetween: 30
+                },
+                1028: {
+                    slidesPerView: 3,
+                    spaceBetween: 30
+                },
+                480: {
+                    slidesPerView: 1,
+                    spaceBetween: 10
+                }
+            }
+        });
+    });
+    
