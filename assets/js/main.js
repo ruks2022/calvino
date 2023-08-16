88d2cd07ccb08f2cc4e7cf8e8e5d5ec0292bcc61
ruks2022 <https://github.com/ruks2022/instagram_clone.git>
2023-08-16 11:49:48 +0530

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,13 @@ $(document).ready(function() {
         });
     });
     
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () =>{
+    if(scrollY >= 188){
+        navbar.classList.add('bg');
+    }
+    else{
+        navbar.classList.remove('bg');
+    }
+})
